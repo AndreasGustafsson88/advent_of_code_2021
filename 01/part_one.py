5d5d3f70800d31ddcf641d80a56a06c049cd67b1
Andreas Gustafsson <andreas.gustafsson88@gmail.com>
2021-12-01 14:02:35 +0100

    pep standards

--- 01/part_one.py
+++ 01/part_one.py
@@ -4,6 +4,7 @@
 
 RAW_INPUT = 'input.txt'
 
+
 def read_input(name: str) -> list[int]:
     """Reads input and converts to int"""
 
@@ -12,7 +13,7 @@ def read_input(name: str) -> list[int]:
 
 
 def count_depth_increase(sonar_sweep: list[int]) -> int:
-    """Return lenght of list that only accepts numbers that are higher than previous"""
+    """Return length of list that only accepts numbers that are higher than previous"""
 
     return len([a for i, a in enumerate(sonar_sweep) if a > sonar_sweep[i - 1]])
 
